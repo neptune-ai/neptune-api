60e1fcb5c8b62000912251e8be1aae49284912f0
Rafal Jankowski <rafal.jankowski@neptune.ai>
2024-06-18 15:13:56 +0200

    Script updated

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
 
+# Show every command and exit on error
 set -ex
 
+# Preserve package structure
 mv src/neptune_api neptune_api
 
+# To generate in the same directory
+INITIAL_DIRECTORY=$(pwd)
 cd ../
 
+# Generate the client
 openapi-python-client update \
     --url $1 \
     --custom-template-path=neptune-api/templates/ \
     --config neptune-api/openapi-generator-config.yaml
 
-cd neptune-api
+# Restore the initial directory
+cd $INITIAL_DIRECTORY
 
+# Restore package structure
 mv neptune_api src/neptune_api
