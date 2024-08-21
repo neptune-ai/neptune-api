03e3a1c29010e2c45f53860915d5a5248f934a24
Rafał Jankowski <rafal.jankowski@neptune.ai>
2024-08-21 18:57:52 +0200

    chore: Added support for detailed error code (#19)

--- src/neptune_api/proto/neptune_pb/ingest/v1/pub/request_status_pb2.pyi
+++ src/neptune_api/proto/neptune_pb/ingest/v1/pub/request_status_pb2.pyi
@@ -8,6 +8,7 @@ import google.protobuf.descriptor
 import google.protobuf.internal.containers
 import google.protobuf.message
 from ..... import google_rpc
+from ..... import neptune_pb
 import sys
 if sys.version_info >= (3, 8):
     import typing as typing_extensions
@@ -25,13 +26,15 @@ class RequestStatus(google.protobuf.message.Message):
         DESCRIPTOR: google.protobuf.descriptor.Descriptor
         CODE_FIELD_NUMBER: builtins.int
         COUNT_FIELD_NUMBER: builtins.int
+        DETAIL_FIELD_NUMBER: builtins.int
         code: google_rpc.code_pb2.Code.ValueType
         count: builtins.int
+        detail: neptune_pb.ingest.v1.ingest_pb2.IngestCode.ValueType
 
-        def __init__(self, *, code: google_rpc.code_pb2.Code.ValueType=..., count: builtins.int=...) -> None:
+        def __init__(self, *, code: google_rpc.code_pb2.Code.ValueType=..., count: builtins.int=..., detail: neptune_pb.ingest.v1.ingest_pb2.IngestCode.ValueType=...) -> None:
             ...
 
-        def ClearField(self, field_name: typing_extensions.Literal['code', b'code', 'count', b'count']) -> None:
+        def ClearField(self, field_name: typing_extensions.Literal['code', b'code', 'count', b'count', 'detail', b'detail']) -> None:
             ...
     CODE_BY_COUNT_FIELD_NUMBER: builtins.int
 
