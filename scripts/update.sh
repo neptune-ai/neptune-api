43fc2851bebaabe4e050c13d2a6587be80f0b8e8
Patryk Gala <patryk.gala@neptune.ai>
2025-02-14 14:40:08 +0100

    feat: Test retrydoc

--- scripts/update.sh
+++ scripts/update.sh
@@ -29,59 +29,60 @@ convert_swagger_to_openapi() {
 
 # Download the Swagger JSON
 download_swagger "$1/api/leaderboard/swagger.json" "leaderboard_swagger.json"
+download_swagger "$1/api/backend/swagger.json" "backend_swagger.json"
+
 
 # Modify the Swagger JSON to support application/x-protobuf
-jq '
-  .paths |= with_entries(
-    .value |= with_entries(
-      if .value.produces? and (.value.produces | index("application/json")) and (.value.produces | index("application/x-protobuf"))
-      then
-        .value.produces = ["application/x-protobuf"]
-      else
-        .
-      end
-    )
-  )
-' leaderboard_swagger.json > tmp/leaderboard_swagger.json && mv tmp/leaderboard_swagger.json leaderboard_swagger.json
+# jq '
+#   .paths |= with_entries(
+#     .value |= with_entries(
+#       if .value.produces? and (.value.produces | index("application/json")) and (.value.produces | index("application/x-protobuf"))
+#       then
+#         .value.produces = ["application/x-protobuf"]
+#       else
+#         .
+#       end
+#     )
+#   )
+# ' leaderboard_swagger.json > tmp/leaderboard_swagger.json && mv tmp/leaderboard_swagger.json leaderboard_swagger.json
 
-jq '
-  .paths |= with_entries(
-    .value |= with_entries(
-      if .value.produces? and (.value.produces | index("application/x-protobuf"))
-      then
-        .value.responses."200".schema = {
-          "type": "string",
-          "format": "binary"
-        }
-      else
-        .
-      end
-    )
-  )
-' leaderboard_swagger.json > tmp/leaderboard_swagger.json && mv tmp/leaderboard_swagger.json leaderboard_swagger.json
+# jq '
+#   .paths |= with_entries(
+#     .value |= with_entries(
+#       if .value.produces? and (.value.produces | index("application/x-protobuf"))
+#       then
+#         .value.responses."200".schema = {
+#           "type": "string",
+#           "format": "binary"
+#         }
+#       else
+#         .
+#       end
+#     )
+#   )
+# ' leaderboard_swagger.json > tmp/leaderboard_swagger.json && mv tmp/leaderboard_swagger.json leaderboard_swagger.json
 
 ## Convert the Swagger JSON to OpenAPI 3.0
 convert_swagger_to_openapi "leaderboard_swagger.json" "leaderboard_openapi.json"
+convert_swagger_to_openapi "backend_swagger.json" "backend_openapi.json"
+
+
+redocly join leaderboard_openapi.json backend_openapi.json -o test.json
+redocly bundle main --output filtered.json --remove-unused-components
+
 # Add license information using jq
-jq '.info.license = {"name": "", "url": ""}' leaderboard_openapi.json > tmp_leaderboard_openapi.json && mv tmp_leaderboard_openapi.json leaderboard_openapi.json
+jq '.info.license = {"name": "", "url": ""}' filtered.json > tmp_leaderboard_openapi.json && mv tmp_leaderboard_openapi.json filtered.json
 
 
 # Generate the client
 openapi-python-client generate \
     --overwrite \
-    --url "$1/api/client/v1/api-docs" \
+    --path "filtered.json" \
     --meta none \
     --custom-template-path=templates/ \
     --config openapi-generator-config.yaml \
     --output-path src/neptune_api
 
-openapi-python-client generate \
-    --overwrite \
-    --meta none \
-    --path "leaderboard_openapi.json" \
-    --custom-template-path=templates/ \
-    --config openapi-generator-config.yaml \
-    --output-path src/neptune_retrieval_api
 
 
 # Clean tmp directories
