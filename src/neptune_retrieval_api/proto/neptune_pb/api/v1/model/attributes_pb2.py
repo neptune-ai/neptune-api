50dbef494c9f55b4fc9f14596e5f04be7ea8ea89
Michał Sośnicki <michal.sosnicki@neptune.ai>
2025-02-20 11:01:56 +0100

    feat(api): generate proto for series-values in retrieval api

--- src/neptune_retrieval_api/proto/neptune_pb/api/v1/model/attributes_pb2.py
+++ src/neptune_retrieval_api/proto/neptune_pb/api/v1/model/attributes_pb2.py
@@ -1,4 +1,5 @@
-"""Generated protocol buffer code."""
+
+'Generated protocol buffer code.'
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import symbol_database as _symbol_database
@@ -9,7 +10,7 @@ DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n(neptune_pb/api/v1
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'neptune_pb.api.v1.model.attributes_pb2', _globals)
-if _descriptor._USE_C_DESCRIPTORS == False:
+if (_descriptor._USE_C_DESCRIPTORS == False):
     _globals['DESCRIPTOR']._options = None
     _globals['DESCRIPTOR']._serialized_options = b'\n0ml.neptune.leaderboard.api.model.proto.generatedP\x01'
     _globals['_PROTOATTRIBUTESSEARCHRESULTDTO']._serialized_start = 117
