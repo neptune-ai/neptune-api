f82c4320cd0080371ddd5437659d558c2dff63d3
Krzysztof Godlewski <krzysztof.godlewski@neptune.ai>
2025-05-23 15:42:54 +0200

    Dockerize OpenAPI generation

--- codegen/generate-proto.sh
+++ codegen/generate-proto.sh
@@ -4,16 +4,23 @@
 
 set -ex
 
+orig_dir="$(pwd)"
+trap 'cd "$orig_dir"' EXIT
+
 # Resolve to absolute path
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "$script_dir"
+
 src_dir="$script_dir/../src/"
 
 docker build -t neptune-api-gen-proto_v3 -f Dockerfile.proto_v3 .
 docker run --rm \
-  -v "$src_dir":"/app/output_dir" \
+  -v "$script_dir/proto":"/app/proto" \
+  -v "$src_dir":"/app/src" \
   neptune-api-gen-proto_v3
 
 docker build -t neptune-api-gen-proto_v4plus -f Dockerfile.proto_v4plus .
 docker run --rm \
-  -v "$src_dir":"/app/output_dir" \
+  -v "$script_dir/proto":"/app/proto" \
+  -v "$src_dir":"/app/src" \
   neptune-api-gen-proto_v4plus
